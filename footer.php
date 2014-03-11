9d01b36a6394ae44e9c872106a907b3da9bb3641
greyaxe90 <Travis@Travis-Newtons-MacBook-Pro.local>
2014-03-10 23:45:19 -0400

    Added additional pages and updated template

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <div id="footer">
+          <div class="container">
+          <p class="muted">&copy; <?php echo(date('Y')); ?> Orlando Meshnet Project.</p>
+	  <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a>
+          </div>
+    </div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/docs.min.js"></script>
+  </body>
+</html>
